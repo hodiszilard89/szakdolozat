854e328549f27fc77de234d34b0a5a7a250d2574
hodiszilard89 <hodiszilard89@gmail.com>
2023-03-04 12:47:39 +0100

    add controller package

--- src/main/java/com/example/hirportal01/controller/PostController.java
+++ src/main/java/com/example/hirportal01/controller/PostController.java
@@ -1,4 +1,20 @@
 package com.example.hirportal01.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class PostController {
+
+    @RequestMapping(path="/movies", method = RequestMethod.GET )
+    public List <String> findAll(){
+        return List.of(
+                "elso",
+                "masodik"
+        );
+
+    }
 }
