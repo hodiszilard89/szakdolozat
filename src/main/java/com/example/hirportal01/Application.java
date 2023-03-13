242d22371f9e3491413460349b9ea7b368281aa2
hodiszilard89 <hodiszilard89@gmail.com>
2023-03-13 20:03:01 +0100

    jdbc implementalása a lokális xampp adatbázishoz

--- src/main/java/com/example/hirportal01/Application.java
+++ src/main/java/com/example/hirportal01/Application.java
@@ -1,13 +1,28 @@
 package com.example.hirportal01;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+import java.io.PrintWriter;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Application.class, args);
+
+		System.out.println();
 	}
 
 }
